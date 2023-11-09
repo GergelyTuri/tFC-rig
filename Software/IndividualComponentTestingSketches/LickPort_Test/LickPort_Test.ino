61d191812a1e33f8f5dd28613fa3935e57b8638d
GergelyTuri <gt2253@columbia.edu>
2023-11-08 20:14:41 -0500

    ignore this

--- Software/IndividualComponentTestingSketches/LickPort_Test/LickPort_Test.ino
+++ Software/IndividualComponentTestingSketches/LickPort_Test/LickPort_Test.ino
@@ -1,20 +1,63 @@
   
 const int LickPin = 12;    // Define lick Pin.
-unsigned long currentTime = 0;      
-int oldLickState = 0;
+const int WaterSolenoidPin = 5;
+
+const unsigned long duration = 200;
+
+unsigned long lastLickTime = 0;
+bool isDispensing = false;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LickPin, INPUT);  //input.
+  pinMode(LickPin, INPUT);
+  pinMode(WaterSolenoidPin, OUTPUT);
+  digitalWrite(WaterSolenoidPin, LOW);
+  digitalWrite(LickPin, LOW);
 }
 
 void loop() {
-  int LickState = digitalRead(LickPin);  // Assess lick state.
-  // Serial.print(LickState);
-  if (LickState != oldLickState) {
-    if (LickState == 1) {
-      Serial.println("Lick, ms=" + String(millis()));  //report the time when licking.
+  int LickState = digitalRead(LickPin);  // Read the lick sensor state.
+
+  if (LickState == HIGH) {  // Lick sensor is active HIGH (change this if your sensor is different).
+    if (!isDispensing && (millis() - lastLickTime >= duration)) {
+      // Lick detected and not dispensing water, and enough time has passed since the last lick.
+      Serial.println("Lick Detected - Dispensing Water");
+      digitalWrite(WaterSolenoidPin, HIGH);  // Open the water solenoid valve.
+      lastLickTime = millis();  // Update the last lick time.
+      isDispensing = true;  // Set the flag to indicate water is being dispensed.
     }
-    oldLickState = LickState;
+  }
+
+  if (isDispensing && (millis() - lastLickTime >= 200)) {
+    // Water has been dispensing for 1 second. You can adjust this duration as needed.
+    digitalWrite(WaterSolenoidPin, LOW);  // Close the water solenoid valve.
+    isDispensing = false;  // Reset the flag.
   }
 }
+
+// void loop() {
+//   int LickState = digitalRead(LickPin);  // Assess lick state.
+//   if (LickState == 1) {
+//     lickTime = millis();
+//     Serial.println("Lick, ms=" + String(millis()));  //report the time when licking.
+//     Serial.println("Water on, ms=" + String(millis()));  //report the time when licking.
+//     LickState = 1;
+//     while (LickState == 1 && !timeOut(lickTime, duration)) {    
+//       digitalWrite(WaterSolenoidPin, HIGH);      
+//       if (timeOut(lickTime, duration)) {
+//         digitalWrite(WaterSolenoidPin, LOW);
+//         LickState = 0;
+//         Serial.println("Water off, ms=" + String(millis()));                    
+//         }
+//     }
+//   }
+// }
+
+// bool timeOut(unsigned long startTime, unsigned long duration) {
+//   if (millis() - startTime > duration) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
