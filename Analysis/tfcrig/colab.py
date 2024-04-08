457383b877fe9aef8794960fbdeb1db0685d5a22
Colin Payne-Rogers <cpaynerogers@cedar.com>
2024-04-08 12:22:47 -0400

    Initial notebook 'setup'

--- Analysis/tfcrig/colab.py
+++ Analysis/tfcrig/colab.py
@@ -1,4 +1,7 @@
+import os
 from dataclasses import dataclass
+
+from google.colab import drive
 
 @dataclass
 class Notebook:
@@ -12,3 +15,29 @@ class Notebook:
     repo_url: str = "https://github.com/GergelyTuri/tFC-rig.git"
     repo_root: str = "/content/tFC-rig"
     repo_branch: str = "cpr/analysis-pipeline"
+    data_root: str = "/gdrive/Shareddrives/Turi_lab/Data/aging_project/"
+
+    def setup(self) -> None:
+        self._mount_google_drive()
+
+    def _mount_google_drive(self) -> None:
+        """
+        Mounts Google Drive to the notebook environment.
+
+        Do we ever need to remount? We can always remount with:
+
+            drive.mount("/gdrive", force_remount=True)
+
+        """
+        drive.mount("/gdrive")
+        self._check_data_root_exists()
+
+    def _check_data_root_exists(self) -> None:
+        """
+        Ensures that the provided `data_root` points to a real path.
+        Only useful if run after having mounted a Google Drive
+        """
+        if not os.path.exists(self.data_root):
+            raise Exception(
+                f"Data folder '{self.data_root}' is not available!!!",
+            )
