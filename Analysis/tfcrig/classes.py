ef3155a6923c7a4d6409a275afec451a44767b88
Gergely Turi <gt2253@cumc.columnbia.edu>
2025-02-23 12:28:03 -0500

    Enhance Session data class with additional properties for header and mouse IDs; update session analysis notebook for improved functionality

--- Analysis/tfcrig/classes.py
+++ Analysis/tfcrig/classes.py
@@ -1,26 +1,37 @@
-"""Data class dealing with the preprocessed session data.
-"""
-from dataclasses import dataclass
+"""Data class dealing with the preprocessed session data."""
+
 import json
-from pathlib import Path
 import os
+from dataclasses import dataclass
 from os.path import join
-import pandas as pd
-import numpy as np
+from pathlib import Path
 from typing import Union
+
+import numpy as np
+import pandas as pd
+
 
 @dataclass
 class Session:
     """
     class for parsing json files that contain the preprocessed session data
     """
+
     session_path: Union[str, Path]
-    
+
     def __post_init__(self):
         if isinstance(self.session_path, str):
             self.session_path = Path(self.session_path)
-    
+
     def _load_json(self):
-        with open(self.session_path, 'r') as f:
+        with open(self.session_path, "r", encoding="utf-8") as f:
             data = json.load(f)
         return data
+
+    @property
+    def session_header(self):
+        return self._load_json()["header"]
+
+    @property
+    def session_mouse_ids(self):
+        return self._load_json()["header"]["mouse_ids"]
